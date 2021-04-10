c13dd5ddf7f6680f893fe93c90c9fafd44eaef55
lazaro <lazaro@sg.srv>
2021-04-09 23:16:31 -0700

    deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:3333';
+//const baseURL = 'http://localhost:3333';
+const baseURL = 'https://webfood-api.herokuapp.com';
 
 const api = axios.create({
   baseURL: baseURL,
